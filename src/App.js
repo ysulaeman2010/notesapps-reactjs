7f508c24b919163acce71957b7a53ab9334ca0b3
ysulaeman2010 <ysulaeman2010@gmail.com>
2021-04-27 20:31:27 +0700

    initial commit, notes app using reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import React, { useState, useEffect } from "react";
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState(JSON.parse(localStorage.notes) || []);
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(()=>{
+    localStorage.setItem("notes",JSON.stringify(notes));
+  },[notes]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "Empty",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete));
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if(note.id === activeNote){
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNotesArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactMarkdown from 'react-markdown';
+
+const Main = ({ activeNote, onUpdateNote }) => {
+  const onEditField = (key, value) => {
+    onUpdateNote({
+        ...activeNote,
+        [key] : value,
+        lastModified: Date.now()
+    })
+  };
+
+  if(!activeNote) return <div className='no-active-note'>No note selected</div>
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input
+          type="text"
+          id="title"
+          value={activeNote.title}
+          onChange={(e) => onEditField("title", e.target.value)}
+          autoFocus
+        />
+        <textarea
+          id="body"
+          value={activeNote.body}
+          onChange={(e) => onEditField("body", e.target.value)}
+          placeholder="Write your note here..."
+        />
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <ReactMarkdown className="markdown-preview">{activeNote.body}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
